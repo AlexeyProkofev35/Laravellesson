55ef8eb15cfe1af81ffac6ccc78acac142a9fa8c
Alexey Prokofev <alexey_prokofev_87@internet.ru>
2023-07-11 21:03:43 +0300

    My lesson2

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App\Http\Controllers;
+declare(strict_types=1);
+namespace app\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -9,4 +10,32 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    public function getNews(int $id = null): array
+{
+    if ($id !== null) {
+        return [
+            'title' => fake()->jobTitle(),
+            'author' => fake()->userName(),
+            'status' => 'ACTIVE',
+            'description' => fake()->text(100),
+            'created_at' => now()->format('d-m-Y H:i'),
+        ];
+            }   
+        
+    
+    $quantityNews = 10;
+    $news = [];
+    for ($i=0; $i < $quantityNews; $i++) {
+$news[] = [
+    'title' => fake()->jobTitle(),
+    'author' => fake()->userName(),
+    'status' => 'ACTIVE',
+    'description' => fake()->text(100),
+    'created_at' => now()->format('d-m-Y H:i'),
+];
+    }
+    return $news;
 }
+
+}
+
