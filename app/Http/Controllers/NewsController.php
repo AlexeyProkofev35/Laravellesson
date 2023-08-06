8176bb06321aed9e1668532c365dc94aae0d5d5f
Alexey Prokofev <alexey_prokofev_87@internet.ru>
2023-08-06 20:32:08 +0300

    My lesson6

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -5,22 +5,30 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 
 class NewsController extends Controller
+
+dd(
+    DB::table('news')
+    ->whereNotBetween('id', [3,6])
+    ->get()
+);
+   public function index(Request $request): View
 {
-    public function index():View
-    {
 return view ('admin.news.index', [
-'news.List' => $this->getNews(),
+'newsList' => News::all(),
 ]);
-        
-}
+}      
+
     public function create():View
     {
-        return view ('admin.news.create');
+        $categories = Category::all();
+        return view ('admin.news.create', [
+        'categories' => $categories,
+        ]);
     }
 
     public function store(Request $request)
     {
         $title = isset($_POST['title']) ? $_POST['title'] : null;
     }
-}
+
 
